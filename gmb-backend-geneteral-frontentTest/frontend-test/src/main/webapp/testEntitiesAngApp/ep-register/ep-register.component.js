b865082665361938cdb96d196752ad02c20a9c93
Michael Brosnan <brosnan.michael@gmail.com>
2016-11-23 09:01:39 +0000

    Got json to array in scope

--- gmb-backend-geneteral-frontentTest/frontend-test/src/main/webapp/testEntitiesAngApp/ep-register/ep-register.component.js
+++ gmb-backend-geneteral-frontentTest/frontend-test/src/main/webapp/testEntitiesAngApp/ep-register/ep-register.component.js
@@ -12,13 +12,47 @@ angular.
     
     var firstN	= ctrl.firstName="";
     
-    ctrl.makeJson = function(){
+    ctrl.submit = function(){
+    	
+    	ctrl.serivcesArray = [];
+    	ctrl.serivces1 = [];
+    	ctrl.serivces2 = [];
+    	ctrl.serivces3 = [];
+    	
+    	var sT1var = ctrl.services.serviceTitle1;
+    	var sB1var = ctrl.services.serviceBlurb1;
+    	
+    	ctrl.serivcesArray.push({"serviceTitle" : sT1var,
+    			"serviceBlurb" : sB1var});
+    	
+    	ctrl.serivcesArray.push({"serviceTitle" : ctrl.services.serviceTitle2,
+			"serviceBlurb" : ctrl.services.serviceBlurb2});
+    	
+/*    	ctrl.serivcesArray.push({"services" : ctrl.serivces1});
+    	ctrl.serivcesArray.push({"services" : ctrl.serivces2});*/
+    	
+/*    	var service1 = {
+    			"serviceTitle" : ctrl.services.serviceTitle1,
+    			"serviceBlurb" : ctrl.services.serviceBlurb1
+    	}
+    	
+    	var service2 = {
+    			"serviceTitle" : ctrl.services.serviceTitle2,
+    			"serviceBlurb" : ctrl.services.serviceBlurb2
+    	}
+    	
+    	ctrl.addServicesIndivdual = function(){
+    		ctrl.serivcesArray.push(ctrl.service1);
+    		ctrl.serivcesArray.push(ctrl.service2);
+    		return servicesArrayComplete;
+    }*/
     	
     	var registerData = {
     			
     			"firstName" : ctrl.firstName,
-    			"lastName"	: ctrl.lastName
-    			
+    			"lastName"	: ctrl.lastName,
+    			"services" 	: ctrl.serivcesArray
+   			
     	};
     	var signupjsondata = JSON.stringify(registerData);
     	
