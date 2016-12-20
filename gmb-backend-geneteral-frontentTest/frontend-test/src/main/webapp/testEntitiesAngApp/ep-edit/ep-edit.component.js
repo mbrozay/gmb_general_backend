5b095ad9b27de574afd068c1aac65eb98f4bb6af
Michael Brosnan <brosnan.michael@gmail.com>
2016-12-20 16:59:46 +0000

    Last commit before refactor

--- gmb-backend-geneteral-frontentTest/frontend-test/src/main/webapp/testEntitiesAngApp/ep-edit/ep-edit.component.js
+++ gmb-backend-geneteral-frontentTest/frontend-test/src/main/webapp/testEntitiesAngApp/ep-edit/ep-edit.component.js
@@ -1,26 +1,29 @@
 'use strict';
 
-// Register `epList` component, along with its associated controller and template
+//Register `epList` component, along with its associated controller and template
 angular.
-  module('epEdit',['ngCookies']).
-  component('epEdit' ,{
-	  templateUrl: 'ep-edit/ep-edit.template.html',
+module('epEdit',['ngCookies']).
+component('epEdit' ,{
+	templateUrl: 'ep-edit/ep-edit.template.html',
 
-	   controller: ['$cookies','EpIndividual','EpCategories','filterFilter',
-		               function EPEditController($cookies,EpIndividual,EpCategories,filterFilter) {
-	    	 var ctrl = this;
-	    	 ctrl.categories = EpCategories.query();
-	    	 var jsondataStringify = {
-	    	            "sessionToken" : $cookies.get('auth'),
-	    	    };
-	    	 EpIndividual.save(jsondataStringify,function(data){
-	     		ctrl.eps = data;	     		
-	     	}, function(error) {
-	     	    ctrl.message = "error";
-	     	}
-	     			)
-		  }
-	   
-		  ]
-	  
-	});
+	controller: ['$cookies','EpIndividual','EpCategories','filterFilter',
+	             function EPEditController($cookies,EpIndividual,EpCategories,filterFilter) {
+		var ctrl = this;
+		ctrl.submit = function(data){
+			 console.log("Submit working");
+			 console.log(data);
+		}
+		ctrl.categories = EpCategories.query();
+		var jsondataStringify = {
+				"sessionToken" : $cookies.get('auth'),
+		};
+		EpIndividual.save(jsondataStringify,function(data){
+			ctrl.eps = data;	     		
+		}, function(error) {
+			ctrl.message = "error";
+		}
+		);
+
+	} 
+	]  
+});
